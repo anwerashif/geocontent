f817915cde64663c9b8f29f3e949f75ca55016b8
Anwer Ashif <33181419+anwerashif@users.noreply.github.com>
2021-08-31 15:30:21 +0600

    added country based redirection

--- plugin.php
+++ plugin.php
@@ -25,18 +25,17 @@ function my_pre_get_posts( $query ) {
         $cat_id = $myterm->term_id;
     }
 
-    if( !is_admin() && !current_user_can('administrator') ){
+    if(!is_admin() && !current_user_can('administrator')){
 
-        if( isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'post' ) {
-            $query->set( 'cat', $cat_id );
-        }
-	
-	// Exclude Nav Menu
-	/* if ($query->get('post_type') == 'nav_menu_item') {
+        // if( isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'post' ) {
+            // $query->set( 'cat', $cat_id );
+        // }
+
+        if ($query->get('post_type') == 'nav_menu_item') {
             return $query;
         } else {
             $query->set( 'cat', $cat_id );
-        } */
+        }
 
     } else {
         //one of the conditions failed - So do nothing new
@@ -49,3 +48,133 @@ function my_pre_get_posts( $query ) {
 }
 
 add_action('pre_get_posts', 'my_pre_get_posts');
+
+/* 
+* Redirecting based on user country United States
+*/
+function redirect_page_en() {
+
+    $geoip = geoip_detect2_get_info_from_current_ip();
+    $name = $geoip->raw['country']['names']['en'];
+
+    if( $name == "United States" && strpos($_SERVER['REQUEST_URI'], "en") == false ) {
+
+        // unset cookies
+        if (isset($_SERVER['HTTP_COOKIE'])) {
+            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+            foreach($cookies as $cookie) {
+                $parts = explode('=', $cookie);
+                $name = trim($parts[0]);
+                setcookie($name, '', time()-1000);
+                setcookie($name, '', time()-1000, '/');
+            }
+        }
+
+        // split this URI by '/'
+        $uriParts = explode('/', $_SERVER['REQUEST_URI']);
+
+        // get all values after 'en'
+        $uriPartsWithoutLang = array_slice($uriParts, 2);
+
+        // prefix the new URI with 'ru' and 
+        // concatinate the remaining array values with '/' again
+        $newUri = '/en/' . implode('/', $uriPartsWithoutLang);
+
+        // print out the link
+        $furl = site_url($newUri);
+
+        // wp_redirect( $furl );
+        wp_safe_redirect( $furl );
+        exit;
+
+    }
+
+}
+
+add_action( 'template_redirect', 'redirect_page_en' );
+
+/* 
+* Redirecting based on user country France
+*/
+function redirect_page_fr() {
+
+    $geoip = geoip_detect2_get_info_from_current_ip();
+    $name = $geoip->raw['country']['names']['en'];
+
+    if( $name == "France" && strpos($_SERVER['REQUEST_URI'], "fr") == false ) {
+
+        // unset cookies
+        if (isset($_SERVER['HTTP_COOKIE'])) {
+            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+            foreach($cookies as $cookie) {
+                $parts = explode('=', $cookie);
+                $name = trim($parts[0]);
+                setcookie($name, '', time()-1000);
+                setcookie($name, '', time()-1000, '/');
+            }
+        }
+
+        // split this URI by '/'
+        $uriParts = explode('/', $_SERVER['REQUEST_URI']);
+
+        // get all values after 'en'
+        $uriPartsWithoutLang = array_slice($uriParts, 2);
+
+        // prefix the new URI with 'ru' and 
+        // concatinate the remaining array values with '/' again
+        $newUri = '/fr/' . implode('/', $uriPartsWithoutLang);
+
+        // print out the link
+        $furl = site_url($newUri);
+
+        wp_redirect( $furl );
+        exit;
+
+    }
+
+}
+
+add_action( 'template_redirect', 'redirect_page_fr' );
+
+/* 
+* Redirecting based on user country Bangladesh
+*/
+function redirect_page_bn() {
+
+    $geoip = geoip_detect2_get_info_from_current_ip();
+    $name = $geoip->raw['country']['names']['en'];
+
+    if( $name == "Bangladesh" && strpos($_SERVER['REQUEST_URI'], "bn") == false ) {
+
+        // unset cookies
+        if (isset($_SERVER['HTTP_COOKIE'])) {
+            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+            foreach($cookies as $cookie) {
+                $parts = explode('=', $cookie);
+                $name = trim($parts[0]);
+                setcookie($name, '', time()-1000);
+                setcookie($name, '', time()-1000, '/');
+            }
+        }
+
+        // split this URI by '/'
+        $uriParts = explode('/', $_SERVER['REQUEST_URI']);
+
+        // get all values after 'en'
+        $uriPartsWithoutLang = array_slice($uriParts, 2);
+
+        // prefix the new URI with 'ru' and 
+        // concatinate the remaining array values with '/' again
+        $newUri = '/bn/' . implode('/', $uriPartsWithoutLang);
+
+        // print out the link
+        $furl = site_url($newUri);
+
+        wp_redirect( $furl );
+        exit;
+
+    }
+
+}
+
+add_action( 'template_redirect', 'redirect_page_bn' );
